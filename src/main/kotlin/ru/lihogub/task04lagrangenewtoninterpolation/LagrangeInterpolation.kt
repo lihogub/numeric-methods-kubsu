255766404f20534631b2d821db12805a8f698301
Lihogub Oleg <oligli@yandex.ru>
2021-10-31 16:22:35 +0300

    seidel LagrangeInterpolation.kt

---
+++ src/main/kotlin/ru/lihogub/task04lagrangenewtoninterpolation/LagrangeInterpolation.kt
@@ -0,0 +1,14 @@
+package ru.lihogub.task04lagrangenewtoninterpolation
+
+fun interpolateLagrangeMethod(x: Double, interpolationNodes: List<Pair<Double, Double>>): Double {
+    var s = 0.0
+    for (k in interpolationNodes.indices) {
+        var product = 1.0
+        for (j in interpolationNodes.indices) {
+            if (k == j) continue
+            product *= (x - interpolationNodes[j].first) / (interpolationNodes[k].first - interpolationNodes[j].first)
+        }
+        s += product * interpolationNodes[k].second
+    }
+    return s
+}
